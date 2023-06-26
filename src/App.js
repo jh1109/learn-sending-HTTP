5033a04b2789246c6860c26daa61f9bcfd69a624
Juhee Noh <nohjuhee227@gmail.com>
2023-06-26 16:44:02 +0900

    LEARN: GET요청으로 영화정보 불러오기(186)

--- src/App.js
+++ src/App.js
@@ -1,31 +1,45 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import MoviesList from './components/MoviesList';
-import './App.css';
+import MoviesList from "./components/MoviesList";
+import "./App.css";
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+  const [movies, setMovies] = useState([]);
+
+  function fetchMoviesHandler() {
+    // fetch(url, [options])
+    // options에 아무것도 넘기지 않으면 기본 GET 메서드로 진행됨
+    // fetch()를 호출하면 네트워크 요청을 보내고 promise가 반환됨
+    fetch("https://swapi.dev/api/films/")
+      .then((response) => {
+        console.log(response);
+        return response.json();
+        // response의 내장 method: json(). promise를 반환함
+        // JSON response 본문을 자바스크립트 형태의 데이터로 바꿔주는 함수
+      })
+      .then((data) => {
+        console.log(data.results);
+        // 원하는 값만 가져오기
+        const transformedMovies = data.results.map((movieData) => {
+          return {
+            id: movieData.episode_id,
+            title: movieData.title,
+            openingText: movieData.opening_crawl,
+            releaseDate: movieData.release_date,
+          };
+        });
+        console.log(transformedMovies);
+        setMovies(transformedMovies);
+      });
+  }
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        <MoviesList movies={movies} />
       </section>
     </React.Fragment>
   );
